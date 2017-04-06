adba6ecff26c386d21a408bc430ae27e1288b3fc
Ban Iulian <ban.iulian.j@gmail.com>
2017-04-06 11:05:51 +0300

    iptables backup option
    added the option to backup and restore initial iptables configuration

--- killswitch.sh
+++ killswitch.sh
@@ -12,8 +12,11 @@ selection=
 until [ "$selection" = "0" ]; do
     echo ""
     echo "ibVPN KillSwitch"
+    echo "Note - it is advisiblht"    
     echo "1 - Enable KillSwitch"
     echo "2 - Disable KillSwitch"
+    echo "3 - Backup current configuration"
+    echo "4 - Restore configuration from backup"    
     echo ""
     echo "0 - exit program"
     echo ""
@@ -23,6 +26,8 @@ until [ "$selection" = "0" ]; do
     case $selection in
         1 ) sudo sh vpnfirewall.sh ;;
         2 ) sudo sh iptables_restore.sh ;;
+        3 ) iptables-save > /opt/iptables.backup ;;
+        4 ) iptables-restore < /opt/iptables.backup ;;
         0 ) exit ;;
         * ) echo "Please enter 1, 2, or 0";
     esac
